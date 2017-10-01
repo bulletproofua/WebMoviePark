809bce51ab8c161a1aaef994c9b3e3552066c460
bulletproofua <yuriy.haydeychuk@gmail.com>
2017-10-01 18:35:58 +0300

    movie-list

--- src/app/helperсomponents/movie-list/movie-list.component.ts
+++ src/app/helperсomponents/movie-list/movie-list.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, ViewChild } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent implements OnInit {
+export class MovieListComponent {
 
-  constructor() { }
+  private serviceSub: Subscription;
+  private data: any[];
 
-  ngOnInit() {
+  _service: any;
+
+  @Input() filter: object;
+  
+  @Input() set service(ref: any) {
+      console.log("ref : ", ref);
+
+      if ( this.filter === undefined ) {
+          this.filter = { order: 'MovieId ASC' };
+      }
+      
+      if (ref) {
+          console.log("ref INNNNN : ", ref);
+          this._service = ref;
+
+          
+          this.serviceSub = this._service.on('change', this.filter ).subscribe(
+              (instances: any) => {
+                  if (instances instanceof Array) {
+                      
+                      if ( this.fields === undefined ) {
+                          this.fields = Object.keys(instances[0]);
+                      }
+                      console.log("result : ", instances);
+                      this.data = instances;
+                  } else {
+                      console.log("errors : ", instances.error);
+                  }
+              },
+              (error: any) => {
+                  console.log("errors : ", error);
+              }
+          );
+      }
+  }
+
+  @Input() options: any = {
+    //
+  };
+  @Input() fields: string[];
+
+  @Output() selected: EventEmitter<any> = new EventEmitter();
+  
+
+  openMovie(context: any) {
+      // this.router.navigateByUrl('/movie/' + context.id);
+      this.selected.emit(context);
   }
 
 }
--- src/app/movie/mainpage/mainpage.component.ts
+++ src/app/movie/mainpage/mainpage.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+
+import { Movies, FireLoopRef } from '../../shared/sdk/models';
+import { RealTime } from '../../shared/sdk/services';
 
 @Component({
   selector: 'app-mainpage',
@@ -6,10 +13,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mainpage.component.scss']
 })
 export class MainpageComponent implements OnInit {
+  serviceRef: FireLoopRef<Movies>;
+  // fields = ["id", "createdAt", "updatedAt"];
 
-  constructor() { }
+  // options = {  
+
+  // };
+
+  constructor(private realTime: RealTime, private router: Router) { }
 
   ngOnInit() {
+    this.realTime.onReady().subscribe(() => this.setup());
+  }
+
+  setup(): void {
+    this.ngOnDestroy();
+
+    this.serviceRef = this.realTime.FireLoop.ref<Movies>(Movies);
+  }
+  ngOnDestroy() {
+    if (this.serviceRef) {
+        this.serviceRef.dispose();
+    }
+  }
+
+  onSelect(event: any) {
+    this.router.navigateByUrl('/Movies/' + event.id);
   }
 
 }
